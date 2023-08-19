5e958907b06c30ba21a9fc003e86946acdd0a2c7
happy8131 <lion8131@naver.com>
2023-08-19 16:56:14 +0900

    prismasSetting mutation user Update

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,12 +1,56 @@
+import { CreateUsernameResponse } from "./../../util/types";
+import { GraphQLContext } from "../../util/types";
+
 const resolvers = {
   Query: {
     searchUsers: () => {},
   },
   Mutation: {
-    createUsername: (_: any, args: { username: string }, context: any) => {
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<CreateUsernameResponse> => {
       const { username } = args;
-      console.log("log APi", username);
-      console.log("HERE IS CONTEXTd", context);
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        return {
+          error: "Not authorize",
+        };
+      }
+
+      const { id: userId } = session.user;
+
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            username,
+          },
+        });
+
+        if (existingUser) {
+          return {
+            error: "존재하는 유저입니다.",
+          };
+        }
+
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+
+        return { success: true };
+      } catch (error: any) {
+        console.log(error);
+        return {
+          error: error?.message,
+        };
+      }
     },
   },
 };
